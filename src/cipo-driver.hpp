0d800b1168d3ea32b1bf09f5819645241adb93bf
carykees98 <trainboy980@gmail.com>
2024-03-04 18:37:45 -0500

    cipo driver comments

--- src/cipo-driver.hpp
+++ src/cipo-driver.hpp
@@ -28,6 +28,11 @@ class RovCommsPeripheral
     void sendChecksum();
 
 public:
+    /**
+     * @brief Construct a new default Rov Comms Peripheral object
+     *
+     * @param ser
+     */
     RovCommsPeripheral(HardwareSerial &ser = DATA_SERIAL)
         : cipo_checksum(0),
           copi_checksum(0),
@@ -46,10 +51,10 @@ public:
     void sendBlocks(const uint8_t data[], size_t length);
 
     /**
-     * @brief
+     * @brief Sends an 8 bit chunk of data to Topside
      *
      * @tparam T Type of data being sent
-     * @param data
+     * @param data An 8 bit chunk of data
      */
     template <typename T>
     void send(const T &data)
@@ -59,11 +64,22 @@ public:
 
     bool readBlocks();
 
+    /**
+     * @brief Checks the current checksum status
+     *
+     * @return true
+     * @return false
+     */
     inline bool checksumGood() const { return copi_checksum_status; }
+
+    /**
+     * @brief Sets the read buffer index back to the beginning of the array
+     *
+     */
     inline void resetReadBuffer() { read_buffer_index = 0; }
 
     /**
-     * @brief
+     * @brief Reads the first item in the read buffer and increases the index
      *
      * @return uint8_t
      */
@@ -80,14 +96,14 @@ public:
     }
 
     /**
-     * @brief
+     * @brief Gets a pointer to a piece of data from the read buffer
      *
-     * @param length
+     * @param length length of the data to read
      * @return uint8_t*
      */
     uint8_t *popReadBuffer(size_t length = 1)
     {
-        uint8_t *data = &read_buffer[read_buffer_index];
+        uint8_t *data = read_buffer + read_buffer_index;
         read_buffer_index += length;
         if (read_buffer_index > READ_BUFFER_LENGTH)
         {
@@ -99,18 +115,19 @@ public:
     }
 
     /**
-     * @brief
+     * @brief Interprets bits from the read buffer and casts them to a T
      *
-     * @tparam T
-     * @return T
+     * @tparam T type of data being read
+     * @return The data read from the read buffer
      */
     template <typename T>
     T popReadBuffer()
     {
-        T data = *(T *)&read_buffer[read_buffer_index];
+        T data = *reinterpret_cast<T *>(&read_buffer[read_buffer_index]);
         // Serial.println("Pop: " + String(data) + " at " + String(read_buffer_index) + " of " + String(READ_BUFFER_LENGTH) + " with size " + String(sizeof(T)) + "");
         read_buffer_index += sizeof(T);
         if (read_buffer_index > READ_BUFFER_LENGTH)
+            ;
         {
             LOGGING_SERIAL.println(F("Read buffer overflow in RovCommsPeripheral::popReadBuffer()"));
             read_buffer_index = 0;
